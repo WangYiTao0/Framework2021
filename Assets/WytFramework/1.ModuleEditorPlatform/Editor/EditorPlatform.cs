82459c3d0957816107116b32ed57612cd7c6ee0b
Wamg <as5818956@gmail.com>
2021-02-25 03:25:15 +0900

    Project Init

---
+++ Assets/WytFramework/1.ModuleEditorPlatform/Editor/EditorPlatform.cs
@@ -0,0 +1,17 @@
+using UnityEditor;
+using UnityEngine;
+
+namespace WytFramework
+{
+    public class EditorPlatform : EditorWindow
+    {
+        /// <summary>
+        /// Open Window
+        /// </summary>
+        [MenuItem("WytFramework/0.EditorModulizationPlatform")]
+        public static void Open()
+        {
+
+        }
+    }
+}
